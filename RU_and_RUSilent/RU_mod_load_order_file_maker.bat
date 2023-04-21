93b9f3f58cbe20e84c52647a0dc163804c5f808f
xsSplater <xssplater@gmail.com>
2023-04-22 01:44:37 +0300

    Added folders

---
+++ RU_and_RUSilent/RU_mod_load_order_file_maker.bat
@@ -0,0 +1,326 @@
+:: Сделано xsSplater под MIT лицензией.
+
+@echo off
+setlocal enableextensions enabledelayedexpansion
+1>nul chcp 65001
+rem Стандартный цвет: белый по чёрному
+color 07
+
+echo.
+echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+echo █ Ищу файл mod_load_order.txt...                               █
+echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+echo.
+timeout /t 2 1>nul
+cls
+
+REM ЕСЛИ MOD_LOAD_ORDER.TXT НЕ СУЩЕСТВУЕТ...
+if NOT exist mod_load_order.txt (
+rem Красный цвет текста
+	color 0C
+	echo.
+	echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+	echo █ ФАЙЛ MOD_LOAD_ORDER.TXT НЕ НАЙДЕН‼                           █
+	echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+	echo.
+timeout /t 2 1>nul
+cls
+
+rem Стандартный цвет: белый по чёрному
+	color 07
+	echo.
+	echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+	echo █ Вы действительно поместили RU_mod_load_order_file_maker.bat  █
+	echo █ в папку MODS?                                                █
+	echo █                                                              █
+	echo █ Если вы уверены, что это папка MODS и всё в порядке...       █
+	echo █ то я создам для вас новый чистый файл mod_load_order.txt?    █
+	echo █                                                              █
+	echo █                                                              █
+	echo █  Y - Да, пожалуйста. Файл удалён/испорчен/отсутствует.       █
+	echo █  N - Нет, я лучше проверю всё ещё раз... ВЫХОД‼              █
+	echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+	echo.
+
+	Choice
+		If Errorlevel 2 Goto NoMLOT
+		If Errorlevel 1 Goto YesMLOT
+)
+
+goto MLOTfinded
+
+rem СОЗАДАВАТЬ НОВЫЙ ФАЙЛ MOD_LOAD_ORDER.TXT?-----------------------------------!
+:YesMLOT
+	cls
+	rem Салатовый цвет текста
+	color 0A
+	echo.>mod_load_order.txt
+	echo.
+	echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+	echo █ ФАЙЛ MOD_LOAD_ORDER.TXT СОЗДАН‼                              █
+	echo █ Нажмите любую клавишу, чтобы продолжить создание списка...   █
+	echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+	echo.
+	pause
+	cls
+goto MLOTfinded
+
+:NoMLOT
+	cls
+	rem Красный цвет текста
+	color 0C
+	echo.
+	echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+	echo █ Скопируйте файл mod_load_order_file_maker_ru.bat             █
+	echo █ в папку MODS и запустите его снова‼                          █
+	echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+	echo.
+	pause
+exit
+rem ----------------------------------------------------------------------------!
+
+rem ЕСЛИ MOD_LOAD_ORDER.TXT СУЩЕСТВУЕТ...
+:MLOTfinded
+if exist mod_load_order.txt (
+	rem Салатовый цвет текста
+	color 0A
+	echo.
+	echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+	echo █ Файл mod_load_order.txt найден‼                              █
+	echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+	echo.
+	timeout /t 2 1>nul
+	cls
+
+rem Стандартный цвет: белый по чёрному
+	color 07
+	echo.
+	echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+	echo █ Создаю резервную копию текущего файла mod_load_order.txt...  █
+	echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+	echo.
+
+rem СОЗДАЁМ РЕЗЕРВНУЮ КОПИЮ ФАЙЛА MOD_LOAD_ORDER.TXT
+	 echo f | XCOPY mod_load_order.txt mod_load_order.txt_%Date:~0,2%.%Date:~3,2%.%Date:~6,4%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,4%_.bak /y /i 1>nul
+	timeout /t 2 1>nul
+
+	rem Салатовый цвет текста
+	color 0A
+	echo.
+	echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+	echo █ ГОТОВО‼                                                      █
+	echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+	echo.
+	timeout /t 1 1>nul
+	cls
+)
+
+rem ЗАПИСЫВАЕМ СТАНДАРТНУЮ АВТОРСКУЮ ШАПКУ ИЗ ФАЙЛА MOD_LOAD_ORDER.TXT
+rem Стандартный цвет: белый по чёрному
+	color 07
+	echo.
+	echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+	echo █ Записываю стандартную шапку в файл mod_load_order.txt...     █
+	echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+	echo.
+	timeout /t 2 1>nul
+
+	(echo -- ################################################################
+	echo -- Enter user mod names below, separated by line.
+	echo -- Order in the list determines the order in which mods are loaded.
+	echo -- Do not rename a mod's folders.
+	echo -- You do not need to include 'base' or 'dmf' mod folders.
+	echo -- ################################################################
+	echo.)>mod_load_order.txt
+
+	rem Салатовый цвет текста
+	color 0A
+	echo.
+	echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+	echo █ ГОТОВО‼                                                      █
+	echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+	echo.
+	timeout /t 2 1>nul
+	cls
+
+rem ЕСЛИ МОД SCOREBOARD НЕ УСТАНОВЛЕН, ПРОПУСКАЕМ ВСЕ ЕГО ПРОВЕРКИ...
+if NOT exist scoreboard goto GOcheck
+
+rem ЕСЛИ МОД SCOREBOARD УСТАНОВЛЕН...
+if exist scoreboard (
+	rem Салатовый цвет текста
+	color 0A
+	echo.
+	echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+	echo █ НАЙДЕН МОД SCOREBOARD‼                                       █
+	echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+	echo.
+	timeout /t 2 1>nul
+	rem Стандартный цвет: белый по чёрному
+	color 07
+	echo.
+	echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+	echo █ Проверяю установлен ли мод ANIMATION EVENTS, необходимый     █
+	echo █ для работы Scoreboard...                                     █
+	echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+	echo.
+	timeout /t 2 1>nul
+	cls
+
+rem ПРОВЕРЯЕМ УСТАНОВЛЕН ЛИ ANIMATION ЕVENTS...
+
+rem ЕСЛИ МОД ANIMATION ЕVENTS НЕ УСТАНОВЛЕН...
+	if NOT exist animation_events (
+		rem Красный цвет текста
+		color 0C
+		echo.
+		echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+		echo █ ОЙ‼ Вы не установили мод Animation Еvents‼                   █
+		echo █ Без него мод Scoreboard не будет работать‼                   █
+		echo █ УСТАНОВИТЕ ANIMATION ЕVENTS И ЗАПУСТИТЕ ЭТОТ ФАЙЛ СНОВА‼     █
+		echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+		echo.
+		pause
+		exit
+	)
+
+rem ЕСЛИ МОД ANIMATION ЕVENTS УСТАНОВЛЕН...
+	if exist animation_events (
+		rem Салатовый цвет текста
+		color 0A
+		echo.
+		echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+		echo █ МОД ANIMATION ЕVENTS НАЙДЕН‼                                 █
+		echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+		echo.
+		timeout /t 2 1>nul
+
+		rem Стандартный цвет: белый по чёрному
+		color 07
+		echo.
+		echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+		echo █ Поднимаю его вверх списка для правильной работы модов‼       █
+		echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+		echo.
+
+		echo animation_events>>mod_load_order.txt
+
+		timeout /t 2 1>nul
+		rem Салатовый цвет текста
+		color 0A
+		echo.
+		echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+		echo █ ГОТОВО‼                                                      █
+		echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+		echo.
+		timeout /t 2 1>nul
+		cls
+	)
+)
+
+:GOcheck
+
+rem ЕСЛИ МОД GRAPHICS OPTIONS НЕ УСТАНОВЛЕН, ПРОПУСКАЕМ ВСЕ ЕГО ПРОВЕРКИ...
+if NOT exist graphics_options goto Mklist
+
+rem ЕСЛИ МОД GRAPHICS OPTIONS УСТАНОВЛЕН...
+if exist graphics_options (
+	rem Салатовый цвет текста
+	color 0A
+	echo.
+	echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+	echo █ НАЙДЕН МОД GRAPHICS OPTIONS‼                                 █
+	echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+	echo.
+	timeout /t 2 1>nul
+	rem Стандартный цвет: белый по чёрному
+	color 07
+	echo.
+	echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+	echo █ Проверяю установлен ли мод SETTINGS EXTENSION, необходимый   █
+	echo █ для работы Graphics Options...                               █
+	echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+	echo.
+	timeout /t 2 1>nul
+	cls
+
+rem ПРОВЕРЯЕМ УСТАНОВЛЕН ЛИ SETTINGS EXTENSION...
+
+rem ЕСЛИ МОД SETTINGS EXTENSION НЕ УСТАНОВЛЕН...
+	if NOT exist settings_extension (
+		rem Красный цвет текста
+		color 0C
+		echo.
+		echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+		echo █ ОЙ‼ Вы не установили мод Settings Extension‼                 █
+		echo █ Без него мод Graphics Options не будет работать‼             █
+		echo █ УСТАНОВИТЕ SETTINGS EXTENSION И ЗАПУСТИТЕ ЭТОТ ФАЙЛ СНОВА‼   █
+		echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+		echo.
+		pause
+		exit
+	)
+
+rem ЕСЛИ МОД SETTINGS EXTENSION УСТАНОВЛЕН...
+	if exist settings_extension (
+		rem Салатовый цвет текста
+		color 0A
+		echo.
+		echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+		echo █ МОД SETTINGS EXTENSION НАЙДЕН‼                               █
+		echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+		echo.
+		timeout /t 2 1>nul
+
+		rem Стандартный цвет: белый по чёрному
+		color 07
+		echo.
+		echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+		echo █ Поднимаю его вверх списка для правильной работы модов‼       █
+		echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+		echo.
+
+		echo settings_extension>>mod_load_order.txt
+
+		timeout /t 2 1>nul
+		rem Салатовый цвет текста
+		color 0A
+		echo.
+		echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+		echo █ ГОТОВО‼                                                      █
+		echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+		echo.
+		timeout /t 2 1>nul
+		cls
+	)
+)
+
+rem СОЗДАЁМ ОСТАЛЬНОЙ СПИСОК МОДОВ, ИСКЛЮЧАЯ BASE, DMF, ANIMATION ЕVENTS и SETTINGS EXTENSION. В АЛФАВИТНОМ ПОРЯДКЕ(ПОКА ЧТО).
+:Mklist
+rem Стандартный цвет: белый по чёрному
+color 07
+echo.
+echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+echo █ Копирую ваш список модов в файл mod_load_order.txt...        █
+echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+echo.
+
+echo.>>mod_load_order.txt
+for /f "tokens=*" %%i in (' dir /b /ad ^|findstr /iv /c:"dmf" /c:"base" 
+/c:"animation_events" /c:"settings_extension"') do set "mydirs_%%~snxi=%%~nxi" 2>nul
+for /f "tokens=2 delims== eol=" %%i in (' set mydirs_ ') do 1>>mod_load_order.txt echo %%i
+
+timeout /t 2 1>nul
+rem Салатовый цвет текста
+color 0A
+echo.
+echo ▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌
+echo █ ФАЙЛ ГОТОВ‼ ПРОВЕРЬТЕ ЕГО, НА ВСЯКИЙ СЛУЧАЙ.                 █
+echo █ ФАЙЛ СЕЙЧАС ОТКРОЕТСЯ АВТОМАТИЧЕСКИ...                       █
+echo ▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌
+echo.
+pause
+
+start mod_load_order.txt
+
+:EOF
